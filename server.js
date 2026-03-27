1cc192e48534c5a30e6eea3b9278429392a0c82d
Mousa <mousaalihamadna@gmail.com>
2026-03-27 10:27:06 +0300

    done with handling errors

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION 💥 Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const mongoose = require('mongoose');
@@ -12,6 +19,15 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log('Database connected!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
