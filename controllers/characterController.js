6fb0d38320b3eae6690507213c9a3fcae60cfe8a
Mousa <mousaalihamadna@gmail.com>
2026-03-27 10:20:13 +0300

    working on error handling

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,221 +1,182 @@
 const Character = require('./../Models/characterModel');
 const ApiFeatures = require('./../utils/apiFeatures');
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
 
 exports.aliasTopBounty = (req, res, next) => {
   req.url = '/?limit=5&sort=-bounty&fields=name,bounty,crew,title';
   next();
 };
 
-exports.getAllCharacters = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new ApiFeatures(Character.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+exports.getAllCharacters = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new ApiFeatures(Character.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    const characters = await features.query;
+  const characters = await features.query;
 
-    res.status(200).json({
-      status: 'success',
-      results: characters.length,
-      requestedAt: req.requestTime,
-      data: {
-        characters,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
+  res.status(200).json({
+    status: 'success',
+    results: characters.length,
+    requestedAt: req.requestTime,
+    data: {
+      characters,
+    },
+  });
+});
+
+exports.getCharacter = catchAsync(async (req, res, next) => {
+  const character = await Character.findById(req.params.id);
+
+  if (!character) {
+    return next(new AppError('No character found with that ID', 404));
   }
-};
 
-exports.getCharacter = async (req, res) => {
-  try {
-    const character = await Character.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      character,
+    },
+  });
+});
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        character,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
+exports.createCharacter = catchAsync(async (req, res, next) => {
+  const newCharacter = await Character.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      character: newCharacter,
+    },
+  });
+});
+
+exports.updateCharacter = catchAsync(async (req, res, next) => {
+  const character = await Character.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!character) {
+    return next(new AppError('No character found with that ID', 404));
   }
-};
 
-exports.createCharacter = async (req, res) => {
-  try {
-    const newCharacter = await Character.create(req.body);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      character,
+    },
+  });
+});
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        character: newCharacter,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err.message,
-    });
+exports.deleteCharacter = catchAsync(async (req, res, next) => {
+  const character = await Character.findByIdAndDelete(req.params.id);
+
+  if (!character) {
+    return next(new AppError('No character found with that ID', 404));
   }
-};
 
-exports.updateCharacter = async (req, res) => {
-  try {
-    const character = await Character.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        character,
+exports.totalBountyPerCrew = catchAsync(async (req, res, next) => {
+  const bountyPerCrew = await Character.aggregate([
+    {
+      $match: { bounty: { $gt: 0 } },
+    },
+    {
+      $group: {
+        _id: '$crew',
+        totalBounty: { $sum: '$bounty' },
+        members: { $sum: 1 },
+        avgBounty: { $avg: '$bounty' },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+    },
+    {
+      $addFields: { crewName: '$_id' },
+    },
+    {
+      $project: { _id: 0 },
+    },
+    {
+      $sort: {
+        totalBounty: -1,
+      },
+    },
+  ]);
 
-exports.deleteCharacter = async (req, res) => {
-  try {
-    await Character.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      bountyPerCrew,
+    },
+  });
+});
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+exports.pirateCrewInfo = catchAsync(async (req, res, next) => {
+  const crew = req.params.crew;
 
-exports.totalBountyPerCrew = async (req, res) => {
-  try {
-    const bountyPerCrew = await Character.aggregate([
-      {
-        $match: { bounty: { $gt: 0 } },
-      },
-      {
-        $group: {
-          _id: '$crew',
-          totalBounty: { $sum: '$bounty' },
-          members: { $sum: 1 },
-          avgBounty: { $avg: '$bounty' },
-        },
-      },
-      {
-        $addFields: { crewName: '$_id' },
-      },
-      {
-        $project: { _id: 0 },
-      },
-      {
-        $sort: {
-          totalBounty: -1,
-        },
-      },
-    ]);
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        bountyPerCrew,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
-
-exports.pirateCrewInfo = async (req, res) => {
-  try {
-    const crew = req.params.crew;
-
-    const crewInfo = await Character.aggregate([
-      {
-        $match: { crew },
-      },
-      {
-        $group: {
-          _id: '$crew',
-          memberNum: { $sum: 1 },
-          memberNames: { $push: '$name' },
-          totalBounty: { $sum: '$bounty' },
-          avgBounty: { $avg: '$bounty' },
-          highestBounty: {
-            $top: {
-              output: { name: '$name', bounty: '$bounty' },
-              sortBy: { bounty: -1 },
-            },
-          },
-          lowestBounty: {
-            $bottom: {
-              output: { name: '$name', bounty: '$bounty' },
-              sortBy: { bounty: -1 },
-            },
-          },
-          numDevilFruitUsers: {
-            $sum: { $cond: [{ $ne: ['$devilFruit', 'none'] }, 1, 0] },
-          },
-          devilFruitUsers: {
-            $push: {
-              $cond: [
-                { $ne: ['$devilFruit', 'none'] },
-                { name: '$name', devilFruit: '$devilFruit' },
-                '$$REMOVE',
-              ],
-            },
-          },
-          numAlive: {
-            $sum: { $cond: [{ $eq: ['$status', 'Alive'] }, 1, 0] },
-          },
-          numDead: {
-            $sum: { $cond: [{ $eq: ['$status', 'Dead'] }, 1, 0] },
+  const crewInfo = await Character.aggregate([
+    {
+      $match: { crew },
+    },
+    {
+      $group: {
+        _id: '$crew',
+        memberNum: { $sum: 1 },
+        memberNames: { $push: '$name' },
+        totalBounty: { $sum: '$bounty' },
+        avgBounty: { $avg: '$bounty' },
+        highestBounty: {
+          $top: {
+            output: { name: '$name', bounty: '$bounty' },
+            sortBy: { bounty: -1 },
           },
         },
+        lowestBounty: {
+          $bottom: {
+            output: { name: '$name', bounty: '$bounty' },
+            sortBy: { bounty: -1 },
+          },
+        },
+        numDevilFruitUsers: {
+          $sum: { $cond: [{ $ne: ['$devilFruit', 'none'] }, 1, 0] },
+        },
+        devilFruitUsers: {
+          $push: {
+            $cond: [
+              { $ne: ['$devilFruit', 'none'] },
+              { name: '$name', devilFruit: '$devilFruit' },
+              '$$REMOVE',
+            ],
+          },
+        },
+        numAlive: {
+          $sum: { $cond: [{ $eq: ['$status', 'Alive'] }, 1, 0] },
+        },
+        numDead: {
+          $sum: { $cond: [{ $eq: ['$status', 'Dead'] }, 1, 0] },
+        },
       },
-      {
-        $addFields: { crewName: '$_id' },
-      },
-      {
-        $project: { _id: 0 },
-      },
-    ]);
+    },
+    {
+      $addFields: { crewName: '$_id' },
+    },
+    {
+      $project: { _id: 0 },
+    },
+  ]);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        crewInfo,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      crewInfo,
+    },
+  });
+});
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -21,6 +23,27 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = (err) => {
+  const value = err.message.match(/"([^"]*)"/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Validation Error. ${errors.join('. ')}`;
+
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -28,6 +51,15 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err, message: err.message, name: err.name };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
+
+  next();
 };
